a3d29ed6d122bfe33d32c7118ec73e21307369ff
Priyal-16 <priyalgupta042@gmail.com>
2025-09-06 00:25:39 +0530

    UI Updates

---
+++ PortFolio-main/src/pages/Index.jsx
@@ -0,0 +1,25 @@
+import Navigation from "@/components/Navigation";
+import HeroSection from "@/components/HeroSection";
+import AboutSection from "@/components/AboutSection";
+import ServicesSection from "@/components/ServicesSection";
+import ProjectsSection from "@/components/ProjectsSection";
+import TeamSection from "@/components/TeamSection";
+import ContactSection from "@/components/ContactSection";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  return (
+    <>
+      <Navigation />
+      <HeroSection />
+      <AboutSection />
+      <ServicesSection />
+      <ProjectsSection />
+      <TeamSection />
+      <ContactSection />
+      <Footer />
+    </>
+  );
+};
+
+export default Index;
